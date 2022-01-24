d4c68d74a7933af5a794aa99f8fae57825ddb61c
VaginakMeloyan <vaxinak.meloyan.99@mail.ru>
2022-01-24 15:02:23 +0300

    change

--- work.py
+++ work.py
@@ -1,47 +1,58 @@
 # Task_1
 
-# class Circle:
-#     def __init__(self, radius):
-#         if not isinstance(radius, (int, float)):
-#             raise TypeError
-#         self.radius = radius
+class Circle:
+    def __init__(self, radius):
+        if not isinstance(radius, (int, float)):
+            raise TypeError
+        self.radius = radius
 
-#     def area(self):
-#         return self.radius ** 2 * 3.14
+    def area(self):
+        return self.radius ** 2 * 3.14
 
-#     def parimeter(self):
-#         return self.radius * 2 * 3.14
+    def parimeter(self):
+        return self.radius * 2 * 3.14
 
-# object_1 = Circle(5)
+object_1 = Circle(5)
 
-# print(object_1.area())
-# print(object_1.parimeter())
+print(object_1.area())
+print(object_1.parimeter())
 
 
 # Task_2
 
-# class Human:
-#     import datetime
-#     yer = datetime.datetime.today()
-#     yer = yer.year
+import datetime
 
-#     def __init__(self, name, surname, age, height, weight):
-#         self.name = name
-#         self.surname = surname
-#         self.age = age
-#         self.height = height
-#         self.weight = weight
 
-#     def present(self):
-#         return self.name, self.surname, self.age, self.height, self.weight
+class Human:
+    # yer = datetime.datetime.today()
+    # yer = yer.year
 
-#     def optimal_weight(self):
-#         return self.height - 100
+    def __init__(self, name, surname, age, height, weight):
+        self.name = name
+        self.surname = surname
+        self.age = age
+        self.height = height
+        self.weight = weight
 
-#     def will_be_100(self):
-#         return f"{self.yer - self.age + 100} you will be 100 "
+    def present(self):
+        return self.name, self.surname, self.age, self.height, self.weight
 
-# object_1 = Human("Vaghinak", "Meloyan", 29, 177, 70)
-# print(object_1.present())
-# print(object_1.optimal_weight())
-# print(object_1.will_be_100())
+    def optimal_weight(self):
+        return self.height - 100
+
+    def year(self):
+        yer = datetime.datetime.today()
+        yer = yer.year
+        return yer
+
+    def will_be_100(self,):
+        return f"{self.year() - self.age + 100} you will be 100 "
+
+
+
+
+
+object_1 = Human("Vaghinak", "Meloyan", 29, 177, 70)
+print(object_1.present())
+print(object_1.optimal_weight())
+print(object_1.will_be_100())
