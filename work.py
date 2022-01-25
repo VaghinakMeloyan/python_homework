54d4f7cf555361d0a5257c5226b6a8455652e45a
VaginakMeloyan <vaxinak.meloyan.99@mail.ru>
2022-01-26 00:30:52 +0300

    change

--- work.py
+++ work.py
@@ -1,58 +1,60 @@
 # Task_1
 
-class Circle:
-    def __init__(self, radius):
-        if not isinstance(radius, (int, float)):
-            raise TypeError
-        self.radius = radius
-
-    def area(self):
-        return self.radius ** 2 * 3.14
-
-    def parimeter(self):
-        return self.radius * 2 * 3.14
-
-object_1 = Circle(5)
-
-print(object_1.area())
-print(object_1.parimeter())
-
-
-# Task_2
-
-import datetime
-
-
-class Human:
-    # yer = datetime.datetime.today()
-    # yer = yer.year
-
-    def __init__(self, name, surname, age, height, weight):
+class Country:
+    def __init__(self, name, continent, *args, **kwargs):
         self.name = name
-        self.surname = surname
-        self.age = age
-        self.height = height
-        self.weight = weight
+        self.continent = continent
+        super().__init__(*args, **kwargs)
+    def present(self):
+        return f"{self.name} and {self.continent}"
+
+class Brand(Country):
+    def __init__(self, brande_name, business_start_date, *args, **kwargs):
+        self.brand_name = brande_name
+        self.business_start_date = business_start_date
+        super().__init__(*args, **kwargs)
+    def present(self):
+        return F"{self.brand_name} and {self.business_start_date}"
+
+
+class Season(Country):
+    def __init__(self, season_name, average_temperatur, *args, **kwargs):
+        self.season_name = season_name
+        self.average_temperatur = average_temperatur
+        super().__init__(*args, **kwargs)
+    def present(self):
+        return f"{self.season_name} and {self.average_temperatur}"
+
+class Product(Season, Brand):
+    def __init__(self, pruduct_name, product_type, product_price, product_quantity, *args, **kwargs):
+        self.product_name = pruduct_name
+        self.product_type = product_type
+        self.product_price = product_price
+        self.product_quantity = product_quantity
+        super().__init__(*args, **kwargs)
 
     def present(self):
-        return self.name, self.surname, self.age, self.height, self.weight
+        return f"presentetion of product will be {self.product_name, self.product_type, self.product_quantity}"
 
-    def optimal_weight(self):
-        return self.height - 100
+    def discount(self):
+        return F"discount price with 20% will be {self.product_price - self.product_price * 20 / 100}"
 
-    def year(self):
-        yer = datetime.datetime.today()
-        yer = yer.year
-        return yer
+    def increase(self):
+        while True:
+            self.product_quantity += 1
+            print(self.product_quantity)
 
-    def will_be_100(self,):
-        return f"{self.year() - self.age + 100} you will be 100 "
+    def decrease(self):
+        while self.product_quantity > 1:
+            self.product_quantity -= 1
+            print(self.product_quantity)
 
 
+object_1 = Product("boots", "sport", 5000, 399, "Summer", 20, "Adidas", 2000, "Armenia", "Asia")
 
+print(Product.__mro__)
 
-
-object_1 = Human("Vaghinak", "Meloyan", 29, 177, 70)
 print(object_1.present())
-print(object_1.optimal_weight())
-print(object_1.will_be_100())
+print(object_1.discount())
+# print(object_1.increase())
+print(object_1.decrease())
