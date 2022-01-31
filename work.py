1376421cc1b5900a040f39aacb39627899fa1473
VaginakMeloyan <vaxinak.meloyan.99@mail.ru>
2022-01-31 15:26:36 +0300

    change

--- work.py
+++ work.py
@@ -1,67 +1,69 @@
+# Task_1
+class Triangle:
+    def __init__(self, first, second, third, *args):
+        super().__init__(*args)
+        self.first = first
+        self.second = second
+        self.third = third
+
+    def perimeter(self):
+        return self.first + self.second + self.third
+
+    def semi_perimeter(self):
+        return self.perimeter() / 2
+
+    def area(self):
+        return  0.5 *  self.semi_perimeter() * (self.semi_perimeter() - self.first) * \
+         (self.semi_perimeter() - self.second) * \
+                (self.semi_perimeter() - self.third)
+
+
+    def sorted(self):
+        # if not isinstance(self.first(int, float)) and isinstance(self.second(int, float)) and isinstance(self.third(int, float)):
+        #     raise TypeError
+        return sorted((self.first, self.second, self.third))
+
+    def is_alike(self, other):
+        if type(other) is not Triangle:
+            raise TypeError
+        sorted_list_1 = self.sorted()
+        sorted_list_2 = other.sorted()
+        return sorted_list_1[0] / sorted_list_2[0] == sorted_list_1[1] / sorted_list_2[1] == sorted_list_1[2] / \
+               sorted_list_2[2]
+
+
+
+object_1 = Triangle(2, 5, 4)
+object_2 = Triangle(8, 10, 4)
+
+
+print(object_1.perimeter())
+print(object_1.semi_perimeter())
+print(object_1.area())
+print(object_1.is_alike(object_2))
+
 # Task_2
 
-class Hotel:
-    def __init__(self, name, place, room_mid, mid_room_price, room_lux, lux_room_price, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.name = name
-        self.place = place
-        self.room_mid = {"room1": "free", "room2": "free", "room3": "free"}
-        self.mid_room_price = mid_room_price
-        self.room_lux = {"room1": "free", "room2": "free", "room3": "free"}
-        self.lux_room_price = lux_room_price
+class Rectangular:
+    def __init__(self, first, second, third, fourth):
+        self.first = first
+        self.second = second
+        self.third = third
+        self.fourth = fourth
 
-    def presentation(self):
-        return self.name, self.place, self.room_mid, self.mid_room_price, self.room_lux, self.lux_room_price
+    def sorted(self):
+        return sorted((self.first, self.second, self.third, self.fourth))
 
-    def booking(self):
-        for i in self.room_mid.keys():
-            self.room_mid[i] = "busy"
-        return print(F"will book the rooms {self.room_mid}")
-
-    def avaliable_room_check(self):
-        check = True
-        for i in self.room_mid.values():
-            if i == "free":
-                check = True
+    def Rectangular_contains(self, other):
+        if type(other) is Rectangular:
+            first_list = self.sorted()
+            second_list = other.sorted()
+            if first_list[0] > second_list[0] and first_list[1] > second_list[1] and \
+                first_list[2] > second_list[2] and first_list[3] > second_list[3]:
+                return "rectangulator_2 is contain in rectangulator_1"
             else:
-                check = False
-        return check
+                return " rectangulator_2 is not contains in rectangulator_1"
+object_1 = Rectangular(2, 2, 4, 4)
+object_2 = Rectangular(4, 4, 8, 8)
 
-    def discount(self):
-        return F"{self.mid_room_price - self.mid_room_price * 30 / 100}"
-
-
-class Taxi(Hotel):
-    def __init__(self, name, car_type, price_for_tour, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.name = name
-        self.car_type = car_type
-        self.price_for_tour = price_for_tour
-
-    def presentation(self):
-        return self.name, self.car_type, self.price_for_tour
-
-    def discount(self):
-        return f"{self.price_for_tour - self.price_for_tour * 15 / 100}"
-
-
-class Tour(Taxi, Hotel):
-    def __init__(self, name,  *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.name = name
-        # self.price_mid = self.mid_room_price + self.price_for_tour
-        # self.price_lux = self.lux_room_price + self.price_for_tour
-
-    def global_presentation(self):
-        return print(F"we have middle services, in price{self.mid_room_price + self.price_for_tour} \n", F"we have lux services in price{self.lux_room_price + self.price_for_tour}")
-
-
-client_1 = Tour("Geghard_tur", "Bmw", "sedan", 10000, "Leran", "geghart", {},
-                20000, {}, 40000, )
-
-# print(Tour.__mro__)
-print(client_1.presentation())
-# print(client_1.booking())
-# print(client_1.discount())
-print(client_1.avaliable_room_check())
-print(client_1.global_presentation())
+print(object_1.Rectangular_contains(object_2))
